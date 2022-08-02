09adde34c291c589c36b3bba3004bda9919af3c8
Alessandro-Salerno <67583725+tzyvoski@users.noreply.github.com>
2022-08-02 13:25:05 +0200

    Swing 01

---
+++ Swing/01/Main.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+import java.awt.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        // Create and initialize new frame
+        JFrame myFrame = new JFrame();                                      // Create JFrame instance
+        myFrame.setSize(500, 400);                              // Set width and height
+        myFrame.setLayout(null);                                            // Use no layout manager
+        myFrame.setVisible(true);                                           // Make the frame visible
+        myFrame.setTitle("Exercise 01");                                    // Set the frame's title
+        myFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);    // Make it so that the program ends when the Window is closed
+
+        // Create and initialize a button
+        JButton myButton = new JButton("Hello, world!");               // Create JButton instance
+        myButton.setBounds(                                                 // Set position and size of the enew button
+                100, 100,
+                256, 32
+        );
+
+        // Add items to the frame to make them visible
+        myFrame.add(myButton);
+    }
+}
